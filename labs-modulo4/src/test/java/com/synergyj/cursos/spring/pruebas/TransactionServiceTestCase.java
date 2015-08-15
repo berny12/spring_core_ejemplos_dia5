33bdc33390cdbc7618821438f0c3626d0924611a
edgar bernal remoto <tecualoyaster@gmail.com>
2015-08-15 13:26:28 -0500

    transacciones
    sin transacciones

--- labs-modulo4/src/test/java/com/synergyj/cursos/spring/pruebas/TransactionServiceTestCase.java
+++ labs-modulo4/src/test/java/com/synergyj/cursos/spring/pruebas/TransactionServiceTestCase.java
@@ -14,6 +14,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import com.synergyj.cursos.spring.service.BusinessException;
@@ -27,7 +28,7 @@ import com.synergyj.cursos.spring.service.TransaccionService;
  */
 
 @RunWith(SpringJUnit4ClassRunner.class)
-// @ContextConfiguration("/sinTransaccionesAppContext.xml")
+@ContextConfiguration("/sinTransaccionesAppContext.xml")
 // @ContextConfiguration("/txAppContext.xml")
 // @ContextConfiguration("/txDetalleAppContext.xml")
 public class TransactionServiceTestCase {
